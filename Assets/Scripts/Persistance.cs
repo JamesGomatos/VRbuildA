80c289950c5edd638d8ed2d86e91c7876580c219
James Gomatos <goarmygomatos@gmail.com>
2016-10-09 15:30:22 -0400

    first commit

---
+++ Assets/Scripts/Persistance.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Persistance : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+		DontDestroyOnLoad (this.gameObject);
+	}
+	
+
+}
---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+
+public class SceneTransition : MonoBehaviour {
+
+	// initialize a variable
+	public ScreenFader screenFader;
+
+	public void StartGame() {
+		StartCoroutine (StartTransition ());
+
+	}
+
+	IEnumerator StartTransition() {
+		
+		screenFader.fadeIn = false;
+		yield return new WaitForSeconds (screenFader.fadeTime);
+		SceneManager.LoadScene (1);
+		screenFader.fadeIn = true;
+		yield return new WaitForSeconds (screenFader.fadeTime);
+
+	}
+}
