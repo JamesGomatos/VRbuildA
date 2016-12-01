417523573e79704744c6453d00b8392b1055f99a
James Gomatos <goarmygomatos@gmail.com>
2016-12-01 08:31:48 -0500

    Changed File structure and added End Scene

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -13,12 +13,46 @@ public class SceneTransition : MonoBehaviour {
 		StartCoroutine (StartTransition ());
 
 	}
+		
+	public void EndGame() {
+		StartCoroutine (EndTransition ());
+
+	}
+
+	public void TitleGame() {
+		StartCoroutine (TitleTransition ());
+
+	}
+
 
 	IEnumerator StartTransition() {
 		
 		screenFader.fadeIn = false;
 		yield return new WaitForSeconds (screenFader.fadeTime);
+		// Based on Where you put the scene in build settings
 		SceneManager.LoadScene (1);
+		screenFader.fadeIn = true;
+		yield return new WaitForSeconds (screenFader.fadeTime);
+
+	}
+
+	IEnumerator EndTransition() {
+
+		screenFader.fadeIn = false;
+		yield return new WaitForSeconds (screenFader.fadeTime);
+		// Based on Where you put the scene in build settings
+		SceneManager.LoadScene (2);
+		screenFader.fadeIn = true;
+		yield return new WaitForSeconds (screenFader.fadeTime);
+
+	}
+
+	IEnumerator TitleTransition() {
+
+		screenFader.fadeIn = false;
+		yield return new WaitForSeconds (screenFader.fadeTime);
+		// Based on Where you put the scene in build settings
+		SceneManager.LoadScene (0);
 		screenFader.fadeIn = true;
 		yield return new WaitForSeconds (screenFader.fadeTime);
 
